ee0427a6e0f17031643e4232ef483dc1c680a751
astrellon <sovereign250@gmail.com>
2025-11-11 09:49:41 +1100

    Fixed up a few build issues and moved Master mixing group

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -5,7 +5,6 @@ using UnityEngine.Audio;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using AYellowpaper.SerializedCollections;
-using UnityEditor.Audio;
 
 #nullable enable
 
